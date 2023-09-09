5198048ae4102ea697e0e9caec6c13867ae498ca
ajstyza <124314058+ajstyza@users.noreply.github.com>
2023-09-08 22:28:48 -0500

    added return functions

--- react_app/JSXVariables.js
+++ react_app/JSXVariables.js
@@ -2,12 +2,37 @@ import React from 'react';
 
 
 // allows you to render HTML elements with logic inside elements.
-function JSXVariables() {
+// function JSXVariables() {
+//     return (
+//         SOMETHING HTML
+//     )
+// };
+
+function returnAboutMe() {
     return (
 
-        SOMETHING HTML
+        <div>
+        <p id = "about-text">Aaron Styza holds an MFA from University of California, Irvine, and a BA from Eckerd College. He is currently transitioning out of academia and into the prviate sector</p>   
+    </div>
     )
+};
 
+returnAboutMe();
 
+function returnPortfolio() {
+    return (
+        <section>
+        <h1>Pick a Card to Browse Portfolio</h1>
+        <div class="grid-container" id="portfolio">
+            <a href="https://ajstyza.github.io/Portfolio-Challenge/"><img src = "./assets/images/games.jpeg"  class="tile"></a>
+            <div class = "centered">My Portfolio</div> 
+            <a href="https://ajstyza.github.io/Password-Generator/"><img src = "./assets/images/games.jpeg" class="tile"></a></img>
+            <div class = "centered" >Random Password Generator</div>
+            <a href="https://ajstyza.github.io/Password-Generator/"><img src = "./assets/images/games.jpeg" class="tile"></a></img>
+            <div class ="centered">No Content</div>
+            <a href="https://ajstyza.github.io/Password-Generator/"><img src = "./assets/images/games.jpeg" class="tile"></a></img>
+            <div class="centered">No Content</div>
+    </section>
 
+    )
 };
